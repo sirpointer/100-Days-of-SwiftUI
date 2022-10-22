ef608dd207acc4013fb7a65cc7a5bd9d8ff3f14d
Nikita Novikov <sirpointer@gmail.com>
2022-10-23 01:25:56 +0400

    Project 15: Accessibility

---
+++ Accessibility/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Nikita Novikov on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
